b8a1571e55e511eccb04a7157a6bef34985d9100
ohtani shimpei <ohtani.shimpei@gmail.com>
2023-05-23 00:14:47 +0900

    add sp nav

--- js/main.js
+++ js/main.js
@@ -4,3 +4,25 @@ TweenMax.from('.main-read', 1, {
     x: -20,
     ease: Expo.easeInOut,
 })
+
+let pcNavLinks = document.querySelectorAll('.pc-nav .a-nav');
+let spNav = document.querySelector('.sp-nav');
+pcNavLinks.forEach(function(link) {
+    let linkCopy = link.cloneNode(true);
+    spNav.appendChild(linkCopy);
+});
+
+document.querySelector('.open').addEventListener('click', function() {
+    this.classList.toggle('active');
+    document.querySelector('.sp-nav').classList.toggle('panelactive');
+    document.querySelector('.circle').classList.toggle('circleactive');
+});
+
+let navLinks = document.querySelectorAll('.sp-nav a');
+for (let i = 0; i < navLinks.length; i++) {
+    navLinks[i].addEventListener('click', function() {
+        document.querySelector('.open').classList.remove('active');
+        document.querySelector('.sp-nav').classList.remove('panelactive');
+        document.querySelector('.circle').classList.remove('circleactive');
+    });
+}
