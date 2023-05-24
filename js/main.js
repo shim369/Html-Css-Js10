6d17abafa6444248b3a54f2c62db4c050f43d571
ohtani shimpei <ohtani.shimpei@gmail.com>
2023-05-24 19:40:37 +0900

    update js & css

--- js/main.js
+++ js/main.js
@@ -1,5 +1,31 @@
+TweenMax.from('.moon', .3, {
+    delay: 3,
+    width: "100%",
+    height: "100%",
+    background: "#eed770",
+    display: "flex",
+    zIndex: 999,
+    borderRadius: 0,
+    opacity: 1,
+    top: "0%",
+    left: "0%",
+    fontSize: "5em",
+})
+
+TweenMax.from('.moon-title', .5, {
+    delay: 2.6,
+    opacity: 1,
+    display: "block",
+})
+
+TweenMax.from('.moon-text', .5, {
+    delay: 3.3,
+    opacity: 0,
+    display: "none",
+})
+
 TweenMax.from('.main-read', 1, {
-    delay: 1,
+    delay: 3.3,
     opacity: 0,
     x: -20,
     ease: Expo.easeInOut,
