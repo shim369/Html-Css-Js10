80699b51a0de237264128e4f083ec0d5253325b9
ohtani shimpei <ohtani.shimpei@gmail.com>
2023-06-04 08:40:03 +0900

    restore

--- js/main.js
+++ js/main.js
@@ -1,14 +1,3 @@
-<<<<<<< HEAD
-TweenMax.from('.cover', 1.5, {
-    width: "100%",
-    height: "100%",
-    background: "#eed770",
-    display: "flex",
-    zIndex: 1000,
-    opacity: 1,
-    top: "0%",
-    left: "0%",
-})
 
 TweenMax.from('.moon', .3, {
     delay: 1.6,
@@ -16,19 +5,13 @@ TweenMax.from('.moon', .3, {
     height: "100%",
     background: "#eed770",
     display: "flex",
-    zIndex: 999,
+    zIndex: 997,
     borderRadius: 0,
     opacity: 1,
     top: "0%",
     left: "0%",
     fontSize: "3em",
     ease: Expo.easeInOut,
-})
-
-TweenMax.from('nav', .3, {
-    delay: 1.9,
-    opacity: 0,
-    y: 20,
 })
 
 TweenMax.from('.moon-title', .3, {
@@ -45,25 +28,35 @@ TweenMax.from('.moon-text', .3, {
     ease: Expo.easeInOut,
 })
 
-TweenMax.from('.open', .5, {
+TweenMax.from('nav', .3, {
     delay: 2.2,
+    opacity: 0,
+    y: 20,
+})
+
+TweenMax.from('.open', .3, {
+    delay: 2.5,
+    zIndex: 0,
     opacity: 0,
 })
 
-TweenMax.from('.sub-read', .5, {
-    delay: 2.2,
+TweenMax.from('.sub-read', .3, {
+    delay: 2.8,
     opacity: 0,
-    y: -20,
+    x: -20,
     ease: Expo.easeInOut,
 })
 
-TweenMax.from('.main-read', .5, {
-    delay: 2.7,
-=======
-TweenMax.from('.logo', 1, {
-    delay: 1,
->>>>>>> aa2adb2e85aede48bb8567371054a82849943c5a
+TweenMax.from('.main-read', .3, {
+    delay: 3.1,
     opacity: 0,
     x: -20,
+    ease: Expo.easeInOut,
+})
+
+TweenMax.from('.scrolldown', .3, {
+    delay: 3.4,
+    opacity: 0,
+    y: -20,
     ease: Expo.easeInOut,
 })
